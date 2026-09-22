44bf3ddda1b6cdfbe1585598088ecba316f7f9d2
inuEnike <inuenike@gmail.com>
2026-09-22 22:43:26 +0100

    updated add business method params

--- client/src/app/dashboard/_components/navigation/TopBar.tsx
+++ client/src/app/dashboard/_components/navigation/TopBar.tsx
@@ -13,6 +13,7 @@ const TopBar = ({
 }) => {
   const { business } = useBusinessBySlug(slug);
   const { isOnline } = useOnlineStatus();
+
   return (
     <nav className="bg-white px-7 lg:px-12 py-5 shadow-xs flex items-center justify-between sticky top-0">
       <div className="flex items-center gap-4">
